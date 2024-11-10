31d7f998e8497f747d71611ad427d2bf4e2ffb9d
rakrisi <radhesingh12321@gmail.com>
2024-11-10 19:03:43 +0530

    Revert "chore: Remove unused icons from calendar component"
    This reverts commit 193d2f76ac06f39fca55229e9ce5bb91fa66f1d4.

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { ChevronLeftIcon, ChevronRightIcon } from '@radix-ui/react-icons';
 import { DayPicker } from 'react-day-picker';
 
 import { cn } from '@/lib/utils';
@@ -55,6 +56,10 @@ function Calendar({
           'aria-selected:bg-accent aria-selected:text-accent-foreground',
         day_hidden: 'invisible',
         ...classNames,
+      }}
+      components={{
+        IconLeft: ({ ...props }) => <ChevronLeftIcon className="h-4 w-4" />,
+        IconRight: ({ ...props }) => <ChevronRightIcon className="h-4 w-4" />,
       }}
       {...props}
     />
